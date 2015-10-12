f29947382670e9650c27ebe958195ead4a80ff6e
Leo Honkanen <leo.o.honkanen@gmail.com>
2015-10-12 16:57:29 +0300

    big refactor to use enum

--- Tennis/src/ohtu/TennisGame.java
+++ Tennis/src/ohtu/TennisGame.java
@@ -2,10 +2,33 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private enum Score {
+        LOVE (0, "Love"),
+        FIFTEEN (1, "Fifteen"),
+        THIRTY (2, "Thirty"),
+        FORTY (3, "Forty"),
+        MORE (4, "Sixty or More");
+        
+        int key;
+        String desc;
+        
+        Score(int key, String desc) {
+            this.key = key;
+            this.desc = desc;
+        }
+        
+        public static Score getFor(int key) {
+            for (Score score : Score.values()) {
+                if (score.key == key) { return score; }
+            }
+            return Score.MORE;
+        }
+    }
+    private Score m_score1 = Score.LOVE;
+    private Score m_score2 = Score.LOVE;
+    private int scoreDiff = 0;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -14,69 +37,42 @@ public class TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals(player1Name)) {
-            m_score1 += 1;
+            m_score1 = Score.getFor(m_score1.key+1);
+            scoreDiff++;
         }
         else if (playerName.equals(player2Name)) {
-            m_score2 += 1;  
+            m_score2 = Score.getFor(m_score2.key+1);  
+            scoreDiff--;
         } 
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        String score;
+        if (m_score1==m_score2 && scoreDiff == 0) {
+            score = getEvenScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        else if (m_score1 == Score.MORE || m_score2 == Score.MORE) {   
+            score = getWinningScore();
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        else {
+            score = m_score1.desc + "-" + m_score2.desc;
         }
         return score;
+    }
+
+    private String getWinningScore() {
+        String score;
+        if (scoreDiff==1) score ="Advantage player1";
+        else if (scoreDiff == -1) score = "Advantage player2";
+        else if (scoreDiff >= 2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String getEvenScore() {
+        if (m_score1 == Score.MORE) {
+            return "Deuce";
+        }
+        return m_score1.desc + "-All";
     }
 }
